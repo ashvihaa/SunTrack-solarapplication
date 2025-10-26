f9cfb7e5c3b4c1c0d26c4014d748c8d0afd26f0a
Gopiraj <gopiraj0208@gmail.com>
2025-10-26 23:22:20 +0530

    added lead_details table

---
+++ SQL usage/Tables/Lead_Details.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Lead_Details]
+(
+    [Id] INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    [LeadId] INT CONSTRAINT fk_LeadDetails_LeadId REFERENCES [Lead](Id) NOT NULL,
+    [SiteVisit_Schedule] DATETIME NOT NULL,
+    [Sitevisit_Photos] NVARCHAR(100) NOT NULL,
+    [Sitevisist_Status] NVARCHAR(100) NOT NULL,
+    [Notes] NVARCHAR MAX
+);
