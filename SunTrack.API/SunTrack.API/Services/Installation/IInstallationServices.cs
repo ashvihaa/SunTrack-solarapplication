df16ec85c2bd92eb9c31388f393d913a90c80724
Gopiraj <gopiraj0208@gmail.com>
2025-10-28 10:46:46 +0530

    added interface for installation screen

--- SunTrack.API/SunTrack.API/Services/Installation/IInstallationServices.cs
+++ SunTrack.API/SunTrack.API/Services/Installation/IInstallationServices.cs
@@ -1,6 +1,11 @@
-﻿namespace SunTrack.API.Services.Installation
+﻿using SunTrack.API.ViewModels.InstallationVM;
+
+namespace SunTrack.API.Services.Installation
 {
     public interface IInstallationServices
     {
+        Task<List<InstallationStatusVM>> GetInstallationStatusAsync(InstallationStatusVM searchVm);
+        Task<bool> AddOrUpdateInstallationStatusAsync(InstallationStatusVM model);
+
     }
 }
--- SunTrack.API/SunTrack.API/obj/Debug/net8.0/SunTrack.API.AssemblyInfo.cs
+++ SunTrack.API/SunTrack.API/obj/Debug/net8.0/SunTrack.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunTrack.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de325c516f8e2e755530bc9642fe455e65877fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cced0a370bc2ec4f4ec3690b2c78fb5fbebba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunTrack.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunTrack.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
