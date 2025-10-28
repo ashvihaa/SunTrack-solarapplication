f7cced0a370bc2ec4f4ec3690b2c78fb5fbebba1
Gopiraj <gopiraj0208@gmail.com>
2025-10-28 10:42:22 +0530

    view model for installation screen is added

---
+++ SunTrack.API/SunTrack.API/ViewModels/InstallationVM/InstallationStatus.cs
@@ -0,0 +1,26 @@
+﻿namespace SunTrack.API.ViewModels.InstallationVM
+{
+    public class InstallationStatusVM
+    {
+        public int? Id { get; set; }
+        public int? CustomerId { get; set; }
+        public int? ProjectId { get; set; }
+        public bool StructureMounting { get; set; }
+        public bool PanelFixing { get; set; }
+        public bool InverterMounting { get; set; }
+        public bool ACDBAndDCDB { get; set; }
+        public bool Earthing { get; set; }
+        public bool ACCable { get; set; }
+        public bool DCCable { get; set; }
+        public bool CivilWorks { get; set; }
+        public bool LightArrester { get; set; }
+        public bool NetMeter { get; set; }
+
+        // For filtering / searching
+        public string? SearchString { get; set; }
+    
+        // For Pagination
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
--- SunTrack.API/SunTrack.API/obj/Debug/net8.0/SunTrack.API.AssemblyInfo.cs
+++ SunTrack.API/SunTrack.API/obj/Debug/net8.0/SunTrack.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SunTrack.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf64eff378c15cb0849b9c62775365519eb4f55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de325c516f8e2e755530bc9642fe455e65877fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SunTrack.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SunTrack.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
